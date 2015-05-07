7db191653e8c370461cbf18435abb46b34a81de8
unbornchikken <gabor.mezo@outlook.com>
2015-05-07 10:04:22 +0200

    minor tweaks

--- cmake/BoostLibInstaller.cmake
+++ cmake/BoostLibInstaller.cmake
@@ -68,7 +68,7 @@ function(boost_lib_installer req_boost_version req_boost_libs)
     message(STATUS "b2 args: ${b2Args}")
 
     # Resolve dependency tree
-    foreach(i RANGE 5)
+    foreach(i RANGE 3)
         foreach(lib ${req_boost_libs})
             list(APPEND req_boost_libs2 ${lib})
             list(APPEND req_boost_libs2 ${${lib}_dep})
@@ -83,58 +83,77 @@ function(boost_lib_installer req_boost_version req_boost_libs)
         if (EXISTS "${install_dir}/libs/${lib}/build/")
             # Has source
             
-            # TODO: Command has to be a dummy if the library file is already exists!
-            
+            # Setup variables
             set(jam_lib boost_${lib}_jam)
             set(boost_lib boost_${lib})
-
-            # Create lib
-            ExternalProject_Add(
-                  "${jam_lib}"
-                  STAMP_DIR "${CMAKE_BINARY_DIR}/boost-${req_boost_version}"
-                  SOURCE_DIR "${install_dir}"
-                  BINARY_DIR "${install_dir}"
-                  CONFIGURE_COMMAND ""
-                  BUILD_COMMAND "${b2_command}" "${b2Args}" --with-${lib}
-                  INSTALL_COMMAND ""
-                  LOG_BUILD ON)
-
-            add_library(${boost_lib} STATIC IMPORTED GLOBAL)
-
             if(lib STREQUAL "test")
-                set(ComponentLibName unit_test_framework)
+                set(lib_name unit_test_framework)
             else()
-                set(ComponentLibName ${lib})
+                set(lib_name ${lib})
             endif()
-
+            
             if(MSVC)
                 if(MSVC11)
-                    set(CompilerName vc110)
+                    set(compiler_name vc110)
                 elseif(MSVC12)
-                    set(CompilerName vc120)
+                    set(compiler_name vc120)
                 elseif(MSVC14)
-                    set(CompilerName vc140)
+                    set(compiler_name vc140)
                 endif()
-
-                set_target_properties(${boost_lib} PROPERTIES
-                    IMPORTED_LOCATION_DEBUG "${install_dir}/${stage_dir}/lib/libboost_${ComponentLibName}-${CompilerName}-mt-gd-${lib_postfix}.lib"
-                    IMPORTED_LOCATION "${install_dir}/${stage_dir}/lib/libboost_${ComponentLibName}-${CompilerName}-mt-${lib_postfix}.lib"
-                    LINKER_LANGUAGE CXX)
+                
+                set(debug_lib_path "${install_dir}/${stage_dir}/lib/libboost_${lib_name}-${compiler_name}-mt-gd-${lib_postfix}.lib")
+                set(lib_path "${install_dir}/${stage_dir}/lib/libboost_${lib_name}-${compiler_name}-mt-${lib_postfix}.lib")
             else()
                 if((CMAKE_BUILD_TYPE STREQUAL "Debug") OR (CMAKE_BUILD_TYPE STREQUAL "") OR (NOT DEFINED CMAKE_BUILD_TYPE))
-                    set_target_properties(${boost_lib} PROPERTIES
-                        IMPORTED_LOCATION "${install_dir}/${stage_dir}/lib/libboost_${ComponentLibName}-mt-d.a"
-                        LINKER_LANGUAGE CXX)
+                    set(lib_path "${install_dir}/${stage_dir}/lib/libboost_${lib_name}-mt-d.a")
                 else()
-                    set_target_properties(${boost_lib} PROPERTIES
-                        IMPORTED_LOCATION "${install_dir}/${stage_dir}/lib/libboost_${ComponentLibName}-mt.a"
-                        LINKER_LANGUAGE CXX)
+                    set(lib_path "${install_dir}/${stage_dir}/lib/libboost_${lib_name}-mt.a")
                 endif()
             endif()
 
-            set_target_properties(${jam_lib} ${boost_lib} PROPERTIES
-                LABELS Boost EXCLUDE_FROM_ALL TRUE)
+            # Create lib
+            if(EXISTS ${lib_path})
+                message(STATUS "Library ${lib} already built.")
+                # Dummy project:
+                ExternalProject_Add(
+                    "${jam_lib}"
+                    STAMP_DIR "${CMAKE_BINARY_DIR}/boost-${req_boost_version}"
+                    SOURCE_DIR "${install_dir}"
+                    BINARY_DIR "${install_dir}"
+                    CONFIGURE_COMMAND ""
+                    BUILD_COMMAND ""
+                    INSTALL_COMMAND ""
+                    LOG_BUILD OFF)
+            else()
+                message(STATUS "Setting up external project to build ${lib}.")
+                ExternalProject_Add(
+                    "${jam_lib}"
+                    STAMP_DIR "${CMAKE_BINARY_DIR}/boost-${req_boost_version}"
+                    SOURCE_DIR "${install_dir}"
+                    BINARY_DIR "${install_dir}"
+                    CONFIGURE_COMMAND ""
+                    BUILD_COMMAND "${b2_command}" "${b2Args}" --with-${lib}
+                    INSTALL_COMMAND ""
+                    LOG_BUILD ON)
+            endif()            
 
+            add_library(${boost_lib} STATIC IMPORTED GLOBAL)            
+
+            if(MSVC)
+                set_target_properties(${boost_lib} PROPERTIES
+                    IMPORTED_LOCATION_DEBUG "${debug_lib_path}"
+                    IMPORTED_LOCATION "${lib_path}"
+                    LINKER_LANGUAGE CXX)
+            else()
+                set_target_properties(${boost_lib} PROPERTIES
+                    IMPORTED_LOCATION "${lib_path}"
+                    LINKER_LANGUAGE CXX)
+            endif()
+
+            # Exlude it from all
+            set_target_properties(${jam_lib} ${boost_lib} PROPERTIES LABELS Boost EXCLUDE_FROM_ALL TRUE)
+
+            # Setup dependencies
             add_dependencies(${boost_lib} ${jam_lib})
             foreach(dep_lib ${${lib}_dep})
                 message(STATUS "Setting ${boost_lib} dependent on boost_${dep_lib}")
@@ -155,12 +174,15 @@ function(boost_lib_installer req_boost_version req_boost_libs)
     endif()
 
     # b2 headers
-    # TODO: Generate only if doesn't exists
-    message(STATUS "Generating headers ...")
-    execute_process(COMMAND ${b2_command} headers WORKING_DIRECTORY ${install_dir} RESULT_VARIABLE err OUTPUT_VARIABLE err_msg)
-    if(err)
-        message(FATAL_ERROR "b2 error:\n${err_msg}")
-    endif(err)
+    if(NOT EXISTS ${install_dir}/boost/)
+        message(STATUS "Generating headers ...")
+        execute_process(COMMAND ${b2_command} headers WORKING_DIRECTORY ${install_dir} RESULT_VARIABLE err OUTPUT_VARIABLE err_msg)
+        if(err)
+            message(FATAL_ERROR "b2 error:\n${err_msg}")
+        endif(err)
+    else()
+        message(STATUS "Headers found.")
+    endif()
 
     set(Boost_INCLUDE_DIRS "${install_dir}" PARENT_SCOPE)
     set(Boost_FOUND TRUE PARENT_SCOPE)
